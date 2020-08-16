d152fd75db7cb6f80bfc07ac3f6d4b62c709cc68
Xiao Chen <seanchen607@gmail.com>
2020-08-15 23:32:29 -0400

    Add files via upload

--- R/FScanR.R
+++ R/FScanR.R
@@ -65,7 +65,7 @@ FScanR <- function(blastx_output    = FScanR:::test_data,
 			qframe_last <- ifelse(qframe_last %in% c(-3, 3), 0, qframe_last)
 			qframe <- ifelse(qframe %in% c(-3, 3), 0, qframe)
 			FS_type <- qframe - qframe_last
-			if (qDist > 0 & sDist > 0 & qDist <= frameDist_cutoff & sDist <= floor(frameDist_cutoff/3)) {
+			if (qDist & sDist > 0 & qDist <= frameDist_cutoff & sDist <= floor(frameDist_cutoff/3)) {
 				prf_sub <- data.frame(as.character(qseqid), frameStart, frameEnd, as.character(sseqid), send_last + 1, sstart, FS_type)
 				prf <- rbind(prf, prf_sub)
 			}
